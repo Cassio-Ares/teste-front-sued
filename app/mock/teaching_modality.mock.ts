3df7aa515c4a75ea85bcd6685d367ff321876456
Cassio-Ares <cassiotadeu.ads@gmail.com>
2025-02-20 16:55:53 -0300

    register

--- app/mock/teaching_modality.mock.ts
+++ app/mock/teaching_modality.mock.ts
@@ -1,7 +1,7 @@
 export const teachingModalities = [
-  "Infantil (0 a 5 anos)",
-  "Fundamental (6 a 14 anos)",
-  "Ensino Medio (15 a 17 anos)",
-  " Educação Especial (Todas as idades)",
+  "Infantil(0 a 5 anos)",
+  "Fundamental(6 a 14 anos)",
+  "Ensino Medio(15 a 17 anos)",
+  "Educação Especial(Todas as idades)",
   "Escola não tem aula neste turno ou não possui esta modalidade",
 ];
