c6f6607b971408f8a0bd0984ba5296075e35c4a4
Cassio-Ares <cassiotadeu.ads@gmail.com>
2025-02-22 15:39:33 -0300

    fix any type User

--- app/admin/(usersData)/users/page.tsx
+++ app/admin/(usersData)/users/page.tsx
@@ -229,7 +229,7 @@ const RegisterUserPage = () => {
     }
   };
 
-  //users any
+  //users
   const [searchUser, setSearchUser] = useState("");
   const { data: userData, data, error, loading, setQuery, refetch } = useSearch<any>("users");
 
