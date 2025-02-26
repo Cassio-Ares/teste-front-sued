f37ecbd3ebe718ebf4d31d5d1705aabd5f563ca8
Cassio-Ares <cassiotadeu.ads@gmail.com>
2025-02-26 09:35:27 -0300

    infantil

--- app/mock/teaching_modality.mock.ts
+++ app/mock/teaching_modality.mock.ts
@@ -1,5 +1,5 @@
 export const teachingModalities = [
-  "Infantil(0 a 5 anos)",
+  " Infantil(0 a 5 anos)",
   "Fundamental(6 a 14 anos)",
   "Ensino Medio(15 a 17 anos)",
   "Educação Especial(Todas as idades)",
