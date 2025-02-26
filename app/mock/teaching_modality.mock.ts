029fb3a56484983602961b73552f465921613307
Cassio-Ares <cassiotadeu.ads@gmail.com>
2025-02-26 14:45:49 -0300

    fix erro teaching modalities

--- app/mock/teaching_modality.mock.ts
+++ app/mock/teaching_modality.mock.ts
@@ -1,7 +1,9 @@
 export const teachingModalities = [
-  " Infantil(0 a 5 anos)",
+  "Infantil(0 a 5 anos)",
   "Fundamental(6 a 14 anos)",
   "Ensino Medio(15 a 17 anos)",
+  "EJA Fundamental(A partir de 15 anos)",
+  "EJA Medio(A partir de 18 anos)",
   "Educação Especial(Todas as idades)",
   "Escola não tem aula neste turno ou não possui esta modalidade",
 ];
